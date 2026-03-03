af2148ef50542a430ff26b05b24ffe19cde5535e
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-03-03 10:10:41 +0100

    Skapat ett grund för projektet med Vite och Sass

---
+++ project/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      }
+    }
+  },
+  plugins: [
+    ViteImageOptimizer({
+
+      webp:{
+        quality: 80
+      }
+
+    })
+  ]
+
+  });
