fda60b6c57b7853f33e9dd574a9cf007a27d7c36
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-03-10 11:44:58 +0100

    Fixat styling på startsida. Strukturerat upp js-fil med vilka funktioner som behövs

---
+++ project/src/js/main.js
@@ -0,0 +1,14 @@
+
+//1. Hämta data om spel från Rawg
+
+//2. Lägg eventlyssnare på sökfunktion. Data som spel ska visas
+
+//3. Skriv ut data i dom med info om spel och knapp till vidare info
+
+//4. Hämta data (summering av spel) från wikipedia
+
+//5. Lägg eventlyssnare på knappen för att visa mer info. Den ska visa summeringen från wiki
+
+//.6 Filtera de 3 bäst rankade spelen
+
+//7. Skriv ut topspelen i dom med spelinfo och knapp till summering (Toppspel ska ligga på startsida vid laddning)
