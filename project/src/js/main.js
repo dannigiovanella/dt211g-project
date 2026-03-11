613a5d240e3fc5bd6f67512d714df5b0018369e3
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-03-11 14:40:34 +0100

    Skapt funktion som tar data från rawg och skapar spelkort med information om spel som visas

--- project/src/js/main.js
+++ project/src/js/main.js
@@ -1,11 +1,12 @@
 "use strict";
 
-
+//APIer (url och nyckel)
 //Rawg API
 const rawgApi = "https://api.rawg.io/api/games";
 const rawgKey = import.meta.env.VITE_RAWGAPIKEY;
-
 //Wiki API
+
+
 
 //Hämta data om spel från Rawg vid sökning av Far Cry
 async function getGames(search = "Far Cry") {
@@ -16,14 +17,14 @@ async function getGames(search = "Far Cry") {
 
         //Visar arrayen i konsol om det funkar
         console.log(rawgData.results);
+        //Skickar data till funkton som skapar spelkort
+        showGames(rawgData.results);
 
         //Vid fel av hämting av data
     } catch (error) {
         console.error("Fel vid hämtning av spel", error);
     }
 }
-//Anropa funktion
-getGames();
 
 
 //Sökfunktion - Eventlyssnare för sökfältet
@@ -32,13 +33,40 @@ const searchBtn = document.querySelector("#searchbtn");
 const searchInput = document.querySelector("#searchinput");
 
 searchBtn.addEventListener("click", () => {
-  const searchValue = searchInput.value.trim();
-  //Kör funktionen för att hämta data med vädret från input vid klick av knapp
-  if (searchValue) {
-    getGames(searchValue);
-  }
+    const searchValue = searchInput.value.trim();
+    //Kör funktionen för att hämta data med vädret från input vid klick av knapp
+    if (searchValue) {
+        getGames(searchValue);
+    }
 });
 
+//Funktion skapar kort som visar varje spel (skapar klass för css)
+// Kort = Bild, spelnamn, releasedatum, knapp för mer info
+
+function showGames(games) {
+    //Hämtar id för div till att visa spel
+    const gameDiv = document.querySelector("#searchresults");
+    //Tömmer listan med sökningar
+    gameDiv.innerHTML = "";
+    //Skapar ett kort (div) med spelinfo och css-klass och knapp
+    games.forEach(game => {
+        const gameCard = document.createElement("div");
+        gameCard.classList.add("gamecard");
+
+        gameCard.innerHTML = `
+      <img src="${game.background_image}" alt="${game.name}">
+      <h3>${game.name}</h3>
+      <p>Released: ${game.released}</p>
+      <button class="showinfo">Show info</button>
+    `;
+        //Lägger till div för spelkort i "searchresults"
+        gameDiv.appendChild(gameCard);
+
+    });
+}
+
+//Anropa funktion för att hämta spel
+getGames();
 
 
 //1. Hämta data om spel från Rawg
